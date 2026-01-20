7013e222b521750d2ff716933ba3ff7423a944df
GermanHv <ghove7@gmail.com>
2026-01-19 20:28:52 -0600

    Revert "Backup exceptions"
    This reverts commit 616797efc2e60a546a542d0ffc165149619c3c18.

--- TaskManager/Models/Category.cs
+++ TaskManager/Models/Category.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool IsDeleted { get; set; } = false;
+
         public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
     }
 }
